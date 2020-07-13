cee089bc496da3d05a932c8dcb53ce00f2fcbd71
Emilien Devos <contact@emiliendevos.be>
2020-07-14 01:25:14 +0200

    add example on how to fetch the status of a private server

---
+++ kubernetes/java/README.md
@@ -0,0 +1,3 @@
+## How to specify the KUBECONFIG?
+Set the environment variable "KUBECONFIG" to the location where your kubeconfig is located.
+Or you can set it in the code but I can't find how to do it, here is the document anyway: https://github.com/fabric8io/kubernetes-client#configuring-the-client
---
+++ kubernetes/java/src/main/java/example/StatusPrivateServer.java
@@ -0,0 +1,38 @@
+package example;
+
+import io.fabric8.kubernetes.client.DefaultKubernetesClient;
+import io.fabric8.kubernetes.client.KubernetesClient;
+import io.fabric8.kubernetes.client.KubernetesClientException;
+import io.fabric8.kubernetes.client.dsl.base.CustomResourceDefinitionContext;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import org.json.JSONObject;
+
+public class StatusPrivateServer {
+    public static void main(String[] args) {
+        try {
+            // Initialize the Kubernetes client library
+            KubernetesClient client = new DefaultKubernetesClient();
+
+            // Custom Resource Definition is a way to interact with a custom block of the
+            // API of k8s provided by an external actor
+            CustomResourceDefinitionContext privateSrvCrdContext = new CustomResourceDefinitionContext.Builder()
+                    .withName("helmreleases.helm.fluxcd.io").withGroup("helm.fluxcd.io").withScope("Namespaced")
+                    .withVersion("v1").withPlural("helmreleases").build();
+
+            // Get full JSON object from the helm release "notch"
+            JSONObject helmReleaseStatus = new JSONObject(
+                    client.customResource(privateSrvCrdContext).get("minecraft", "notch"));
+            // If you get "Succeeded" then the helm release is ready and so is the private server.
+            System.out.println(helmReleaseStatus.getJSONObject("status").get("phase"));
+
+            // Close the kubernetes client (we can't reuse it after that) - Optional because
+            // the library close it itself
+            client.close();
+        } catch (KubernetesClientException e) {
+            e.printStackTrace();
+        }
+    }
+}
