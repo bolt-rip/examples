b10a53987e3dd94b8f134041fb5cb31b3a40200d
Emilien Devos <contact@emiliendevos.be>
2020-07-16 16:56:08 +0200

    deploy a private server with valid mc username

--- kubernetes/java/src/main/java/example/DeployPrivateServer.java
+++ kubernetes/java/src/main/java/example/DeployPrivateServer.java
@@ -29,18 +29,18 @@ public class DeployPrivateServer {
                     .load(new FileInputStream(new File("src/main/resources/DeployPrivateServer.yaml")));
 
             // Load the helmrelease as a RAW json - Not good because hardcoding
-            String helmReleaseObjectFromRAWJson = "{\"metadata\":{\"name\":\"indicado\"},\"apiVersion\":\"helm.fluxcd.io/v1\","
+            String helmReleaseObjectFromRAWJson = "{\"metadata\":{\"name\":\"mitchiii--server\"},\"apiVersion\":\"helm.fluxcd.io/v1\","
                     + "\"kind\":\"HelmRelease\",\"spec\":{\"chart\":{\"path\":\"charts/privateserver\",\"ref\":\"master\",\"git\""
                     + ":\"git@github.com:bolt-rip/k8s\"},\"values\":{\"operators\":\"notch:jeb_\",\"maxPlayers\":20}}}";
+
+            String username = "mitchiii_";
 
             // Create a JSONObject in order to programmatically add a server name, operator
             // name and so on.
             JSONObject helmReleaseJSONObject = new JSONObject(helmReleaseObjectFromYAML);
-            helmReleaseJSONObject.getJSONObject("metadata").put("name", "notch");
-            // it is needed to change it twice because without that the server name would be "minecraft-notch"
-            // more info about that in https://docs.fluxcd.io/projects/helm-operator/en/stable/references/helmrelease-custom-resource/
-            helmReleaseJSONObject.getJSONObject("spec").put("releaseName", "notch");
-            helmReleaseJSONObject.getJSONObject("spec").getJSONObject("values").getJSONObject("config").put("operators", "notch");
+            helmReleaseJSONObject.getJSONObject("metadata").put("name", username.toLowerCase().replaceAll("_", "-") + "-server");
+            helmReleaseJSONObject.getJSONObject("spec").getJSONObject("values").getJSONObject("config").put("serverName", username);
+            helmReleaseJSONObject.getJSONObject("spec").getJSONObject("values").getJSONObject("config").put("operators", username);
 
             // Send the JSON Object to the k8s API
             client.customResource(privateSrvCrdContext).create("minecraft", helmReleaseJSONObject.toString());
