efda18e31490e5d3c6f1c2d49fd92ada4b057535
Emilien Devos <contact@emiliendevos.be>
2021-01-14 21:23:00 +0100

    update example due to the drop of fluxcd

--- kubernetes/java/src/main/java/example/DeployPrivateServer.java
+++ kubernetes/java/src/main/java/example/DeployPrivateServer.java
@@ -21,29 +21,29 @@ public class DeployPrivateServer {
             // Custom Resource Definition is a way to interact with a custom block of the
             // API of k8s provided by an external actor
             CustomResourceDefinitionContext privateSrvCrdContext = new CustomResourceDefinitionContext.Builder()
-                    .withName("helmreleases.helm.fluxcd.io").withGroup("helm.fluxcd.io").withScope("Namespaced")
-                    .withVersion("v1").withPlural("helmreleases").build();
+                    .withName("helmcharts.helm.cattle.io").withGroup("helm.cattle.io").withScope("Namespaced")
+                    .withVersion("v1").withPlural("helmcharts").build();
 
-            // Load the helmrelease from a file - Good because no hardcoding
-            Map<String, Object> helmReleaseObjectFromYAML = client.customResource(privateSrvCrdContext)
+            // Load the helmChart from a file - Good because no hardcoding
+            Map<String, Object> helmChartObjectFromYAML = client.customResource(privateSrvCrdContext)
                     .load(new FileInputStream(new File("src/main/resources/DeployPrivateServer.yaml")));
 
-            // Load the helmrelease as a RAW json - Not good because hardcoding
-            String helmReleaseObjectFromRAWJson = "{\"metadata\":{\"name\":\"mitchiii--server\"},\"apiVersion\":\"helm.fluxcd.io/v1\","
-                    + "\"kind\":\"HelmRelease\",\"spec\":{\"chart\":{\"path\":\"charts/privateserver\",\"ref\":\"master\",\"git\""
+            // Load the helmChart as a RAW json - Not good because hardcoding - lines 32-34 is an old example that doesn't work anymore
+            String helmChartObjectFromRAWJson = "{\"metadata\":{\"name\":\"mitchiii--server\"},\"apiVersion\":\"helm.cattle.io/v1\","
+                    + "\"kind\":\"HelmChart\",\"spec\":{\"chart\":{\"path\":\"charts/privateserver\",\"ref\":\"master\",\"git\""
                     + ":\"git@github.com:bolt-rip/k8s\"},\"values\":{\"operators\":\"notch:jeb_\",\"maxPlayers\":20}}}";
 
             String username = "mitchiii_";
 
             // Create a JSONObject in order to programmatically add a server name, operator
             // name and so on.
-            JSONObject helmReleaseJSONObject = new JSONObject(helmReleaseObjectFromYAML);
-            helmReleaseJSONObject.getJSONObject("metadata").put("name", "private-" + username.toLowerCase().replaceAll("_", "-") + "-server");
-            helmReleaseJSONObject.getJSONObject("spec").getJSONObject("values").getJSONObject("config").put("serverName", username);
-            helmReleaseJSONObject.getJSONObject("spec").getJSONObject("values").getJSONObject("config").put("operators", username);
+            JSONObject helmChartJSONObject = new JSONObject(helmChartObjectFromYAML);
+            helmChartJSONObject.getJSONObject("metadata").put("name", "private-" + username.toLowerCase().replaceAll("_", "-") + "-server");
+            helmChartJSONObject.getJSONObject("spec").getJSONObject("values").getJSONObject("set").put("config.serverName", username);
+            helmChartJSONObject.getJSONObject("spec").getJSONObject("values").getJSONObject("set").put("config.operators", username);
 
             // Send the JSON Object to the k8s API
-            client.customResource(privateSrvCrdContext).create("minecraft", helmReleaseJSONObject.toString());
+            client.customResource(privateSrvCrdContext).create("minecraft", helmChartJSONObject.toString());
 
             // Close the kubernetes client (we can't reuse it after that) - Optional because
             // the library close it itself
